c5107562a862554eb4f183375573c2050fb14c4e
Govind Sharma <150214417+govindsharma56@users.noreply.github.com>
2026-07-29 15:52:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 function createTable() {
     //Write your code here
+	let table=document.querySelector('#myTable');
+	let row=Number(prompt('Input number of rows'));
+	let column=Number(prompt('Input number of column'));
+	if(row<=0){
+		alert('cannot create table');
+		return;
+   }
+	if(rows === null || rows.trim() === "" || Number.isNan(row)){
+		return;
+	}
+	 for(let i=0;i<row;i++){
+		 let tr=document.createElement('tr');
+		 for(let j=0;j<column;j++){
+			 let td=document.createElement('td');
+			 td.textContent=`Row-${i} Column-${j}`;
+			 tr.append(td);
+    }
+		 table.append(tr);
+   }
   
 }
